f57726d4606efe40577326d6d2339b11d209aa79
Jni <jni.viens@gmail.com>
2014-10-25 11:43:26 -0400

    Created a ci

---
+++ ci.sh
@@ -0,0 +1,50 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  ci.sh
+# 
+#         USAGE:  ./ci.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: YOUR NAME (), 
+#       COMPANY: 
+#       CREATED: 25/10/14 10:50:29 AM EDT
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# Bash sucks at figuring itself out. Finds this script's directory. From : http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+FAILING=false
+command -v notify-send > /dev/null && NOTIFY='notify-send -a LiveFeedback -t 2000 -u' || NOTIFY='echo'
+
+inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' --exclude '.*\.swp' -e modify "$DIR/config" "$DIR/mix.exs" "$DIR/lib" "$DIR/test" "$DIR/web" | while read date time dir file; do
+  FILECHANGE=${dir}${file}
+  echo "At ${time} on ${date}, file $FILECHANGE"
+
+
+  /bin/bash $DIR/test.sh quick
+  if [[ "$?" -ne "0" ]]; then
+    FAILING=true
+    $NOTIFY "critical" "Test Failure" 'You broke something again!!'
+  else
+    if $FAILING; then
+      $NOTIFY "normal" "Everything back to normal" "You are awesome"
+    fi
+    FAILING=false
+  fi
+done
+
---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  start.sh
+#
+#         USAGE:  ./start.sh
+#
+#   DESCRIPTION:  Starts a development docker. Uses port 8080
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  A docker image name live_feedback. Use "docker built -t live_feedback ." after copying the proper Dockerfile.* to Dockerfile
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Jni
+#       COMPANY:
+#       CREATED: 19/10/14 03:55:28 PM EDT
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# Bash sucks at figuring itself out. Finds this script's directory. From : http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+RSYNC="rsync --exclude=_build/ --exclude=deps/ --exclude=.git/ --exclude=_test/ --exclude=/Dockerfile* -ar --update --info=progress2"
+
+TEST_MODE=${1:-}
+if [[ "$TEST_MODE" = 'quick' ]]; then
+  echo "Running in quick mode (no docker)"
+  mkdir -p "$DIR/_test/src_quick"
+  $RSYNC "$DIR/" "$DIR/_test/src_quick/"
+  cd "$DIR/_test/src_quick"
+  if [[ ! -d "$DIR/_test/src_quick/deps" ]]; then
+    MIX_ENV=test mix do deps.get, compile
+  fi
+
+  MIX_ENV=test mix test
+else
+  mkdir -p "$DIR/_test/src"
+  $RSYNC "$DIR/" "$DIR/_test/src/"
+  if [[ ! -f "$DIR/_test/Dockerfile" ]] || ! cmp "$DIR/Dockerfile.test" "$DIR/_test/Dockerfile" > /dev/null 2>&1; then
+    cp "$DIR/Dockerfile.test" "$DIR/_test/Dockerfile"
+    cp -R "$DIR/mix_base" "$DIR/_test/mix_base"
+    docker build -t live_feedback_test "$DIR/_test"
+  fi
+
+  docker run -t -v "${DIR}/_test/src:/src" -p 8081:8080 live_feedback_test
+fi
