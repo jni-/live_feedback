16a180ac171369a7479fb8be275b47f2df090d77
Jni <jni.viens@gmail.com>
2014-10-26 17:56:08 -0400

    added debug info

--- src/web/views/layout_view.ex
+++ src/web/views/layout_view.ex
@@ -2,16 +2,20 @@ defmodule LiveFeedback.LayoutView do
   use LiveFeedback.View
 
   def version_id do
-    case System.cmd("cat", ["/proc/self/cgroup"]) do
+    IO.puts "Reading file"
+    version = case System.cmd("cat", ["/proc/self/cgroup"]) do
       {cgroup_info, 0} ->
         version_id cgroup_info
       _ ->
         "Unable to read version"
     end
+    IO.puts "Done reading file"
+    version
   end
 
   def version_id(cgroup_info) do
-    cgroup_info
+    IO.puts "Starting to read string"
+    version = cgroup_info
       |> String.split("\n")
       |> Enum.filter(fn(x) -> String.contains?(x, "docker-") end)
       |> Enum.at(0)
@@ -19,6 +23,8 @@ defmodule LiveFeedback.LayoutView do
       |> Enum.at(1)
       |> String.split(".scope")
       |> Enum.at(0)
+    IO.puts "Done reading string"
+    version
   end
 
 end
