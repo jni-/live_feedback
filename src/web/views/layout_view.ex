a64c4e20eb41e588a798239884cc2f7b49aa9548
Jni <jni.viens@gmail.com>
2014-10-26 19:26:05 -0400

    Debug info since server's docker version is too old for exec

--- src/web/views/layout_view.ex
+++ src/web/views/layout_view.ex
@@ -15,16 +15,35 @@ defmodule LiveFeedback.LayoutView do
 
   def version_id(cgroup_info) do
     IO.puts "Starting to read string"
-    version = cgroup_info
-      |> String.split("\n")
-      |> Enum.filter(fn(x) -> String.contains?(x, "docker-") end)
-      |> Enum.at(0)
-      |> String.split("docker-")
-      |> Enum.at(1)
-      |> String.split(".scope")
-      |> Enum.at(0)
+    IO.inspect cgroup_info
+    IO.puts "------- 1"
+
+    version_lines = cgroup_info |> String.split("\n")
+    IO.inspect version_lines
+    IO.puts "------- 2"
+
+    docker_version_lines = version_lines |> Enum.filter(fn(x) -> String.contains?(x, "docker-") end)
+    IO.inspect docker_version_lines
+    IO.puts "------- 3"
+
+    first_docker_version = docker_version_lines |> Enum.at(0)
+    IO.inspect first_docker_version
+    IO.puts "------- 4"
+
+    extract_after_docker = first_docker_version |> String.split("docker-")
+    IO.inspect extract_after_docker
+    IO.puts "------- 5"
+
+    second_part = extract_after_docker |> Enum.at(1)
+    IO.inspect second_part
+    IO.puts "------- 6"
+
+    extract_before_scope = second_part |> String.split(".scope")
+    IO.inspect extract_before_scope
+    IO.puts "------- 7"
+
     IO.puts "Done reading string"
-    version
+    extract_before_scope |> Enum.at(0)
   end
 
 end
