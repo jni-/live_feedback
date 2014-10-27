5202fadfd0ce9fd1b5e7973bad92c58362c4a230
Jni <jni.viens@gmail.com>
2014-10-26 20:02:23 -0400

    Support version matching for docker 1.14 and 1.15

--- src/test/web/views/layout_view_test.exs
+++ src/test/web/views/layout_view_test.exs
@@ -3,14 +3,29 @@ defmodule LiveFeedback.LayoutViewTest do
 
   alias LiveFeedback.LayoutView
 
-  @cgroup_info """
+  @cgroup_info_1_15 """
     8:bfqio:/
     7:blkio:/system.slice/docker-7a21ae39ee1e0c32d53da3361892257aeafff1ceeaeb9fea91dfae14acbae58b.scope
   """
 
+  @cgroup_info_1_14 """
+    10:perf_event:/docker/7a21ae39ee1e0c32d53da3361892257aeafff1ceeaeb9fea91dfae14acbae58b
+    9:memory:/docker/7a21ae39ee1e0c32d53da3361892257aeafff1ceeaeb9fea91dfae14acbae58b
+    8:hugetlb:/
+  """
+
   @version_from_cgroup "7a21ae39ee1e0c32d53da3361892257aeafff1ceeaeb9fea91dfae14acbae58b"
 
-  test "can extract version from cgroup info" do
-    assert LayoutView.version_id(@cgroup_info) == @version_from_cgroup
+  test "can extract version from cgroup info version 1.15" do
+    assert LayoutView.version_id(@cgroup_info_1_15) == @version_from_cgroup
   end
+
+  test "can extract version from cgroup info version 1.14" do
+    assert LayoutView.version_id(@cgroup_info_1_14) == @version_from_cgroup
+  end
+
+  test "returns an error message if unable to read version string" do
+    assert LayoutView.version_id("not a docker version") == "Unable to read version"
+  end
+
 end
--- src/web/views/layout_view.ex
+++ src/web/views/layout_view.ex
@@ -2,48 +2,21 @@ defmodule LiveFeedback.LayoutView do
   use LiveFeedback.View
 
   def version_id do
-    IO.puts "Reading file"
-    version = case System.cmd("cat", ["/proc/self/cgroup"]) do
+    case System.cmd("cat", ["/proc/self/cgroup"]) do
       {cgroup_info, 0} ->
         version_id cgroup_info
       _ ->
         "Unable to read version"
     end
-    IO.puts "Done reading file"
-    version
   end
 
   def version_id(cgroup_info) do
-    IO.puts "Starting to read string"
-    IO.inspect cgroup_info
-    IO.puts "------- 1"
-
-    version_lines = cgroup_info |> String.split("\n")
-    IO.inspect version_lines
-    IO.puts "------- 2"
-
-    docker_version_lines = version_lines |> Enum.filter(fn(x) -> String.contains?(x, "docker-") end)
-    IO.inspect docker_version_lines
-    IO.puts "------- 3"
-
-    first_docker_version = docker_version_lines |> Enum.at(0)
-    IO.inspect first_docker_version
-    IO.puts "------- 4"
-
-    extract_after_docker = first_docker_version |> String.split("docker-")
-    IO.inspect extract_after_docker
-    IO.puts "------- 5"
-
-    second_part = extract_after_docker |> Enum.at(1)
-    IO.inspect second_part
-    IO.puts "------- 6"
-
-    extract_before_scope = second_part |> String.split(".scope")
-    IO.inspect extract_before_scope
-    IO.puts "------- 7"
-
-    IO.puts "Done reading string"
-    extract_before_scope |> Enum.at(0)
+    case Regex.run(~r/[0-9a-f]{64}/, cgroup_info) do
+      [first_match|rest] ->
+        first_match
+      nil ->
+        "Unable to read version"
+    end
   end
 
 end
