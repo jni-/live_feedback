555925b4ef1f042896e880b2a36f15f7b0a26436
Jni <jni.viens@gmail.com>
2014-10-29 15:36:09 -0400

    Better survival to disconnects

--- src/priv/static/js/live-feedback.js
+++ src/priv/static/js/live-feedback.js
@@ -13,6 +13,18 @@ $(function() {
     return localStorage && parseInt(localStorage.getItem(emotion)) || 5;
   }
 
+  function activateControls(activate) {
+    $('input#appreciation-slider, input#lost-slider').slider(activate ? "enable" : "disable");
+  }
+
+  function reloadPageIfVersionChanged(current_version) {
+    $.getJSON("/version").then(function(data) {
+      if(current_version != data.version) {
+        location.reload();
+      }
+    });
+  }
+
   $('input#appreciation-slider').slider({
     value: getDefaultEmotionValue("appreciation")
   }).on("slideStop", registerEmotion("appreciation"));
@@ -21,25 +33,37 @@ $(function() {
     value: getDefaultEmotionValue("lost")
   }).on("slideStop", registerEmotion("lost"));
 
+
+
   var socket = new Phoenix.Socket("/ws");
+  var socketWasDead = false;
   socket.join("generic", "global", {}, function(channel) {
 
+    if(socketWasDead) {
+      reloadPageIfVersionChanged($('p.version em'));
+      activateControls(true);
+      toastr.clear();
+      toastr.success("Everything is back to normal!");
+    }
+
     channel.on("reload", function(message) {
-      $('input#appreciation-slider, input#lost-slider').slider("disable");
+      activateControls(false);
       toastr.warning("The application is updating, please wait. This should only take a few seconds, your work has been saved.");
 
       setInterval(function() {
-        $.getJSON("/version").then(function(data) {
-          console.log("data : " + data.version);
-          console.log("msg : " + message.current_version);
-          if(message.current_version != data.version) {
-            location.reload();
-          }
-        });
+        reloadPageIfVersionChanged(message.current_Version);
       }, 100);
 
     });
 
+  });
+
+  socket.onClose(function() {
+      activateControls(false);
+      if(!socketWasDead) {
+        toastr.error("The server seems to be going through some rough times.", "Please be supportive in this moment of hardsip.", {timeOut: 0});
+      }
+      socketWasDead = true;
   });
 
 
